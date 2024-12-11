de027089cf6455c062db8646c6bc9e12fd24a19a
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-11 21:46:25 +0530

    Added:Ability to append elements

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -13,6 +13,20 @@ public class LinkedList<T> {
         head = newNode;
         size++;
     }
+    public void append(T data) {
+        Node<T> newNode = new Node<>(data);
+        
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
     public String toString() {
         StringBuilder sb = new StringBuilder();
         Node<T> current = head;
--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -2,9 +2,12 @@
 class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList<Integer> list = new LinkedList<>();
-		list.add(70);
+		/*list.add(70);
         list.add(30);
-        list.add(56);
+        list.add(56);*/
+		list.append(56);
+        list.append(30);
+        list.append(70);
         System.out.println(list.toString());
 	}
 }
